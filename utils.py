08aaf7e8914a904d6df68e3feab57cd7c7b9ca26
Subhankar Ray <7937894+econ-drl@users.noreply.github.com>
2024-06-02 23:12:58 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -144,8 +144,11 @@ def evaluate_agent(agent, test_states, test_bm_preds, test_y):
     act_counter = Counter(weights.argmax(1))
     act_sorted  = sorted([(k, v) for k, v in act_counter.items()])
     weights = np.expand_dims(weights, -1)  # (2816, 9, 1)
+    print (weights.shape); #sray
+    print (test_bm_preds); #sray
     weighted_y = weights * test_bm_preds  # (2816, 9, 24)
     weighted_y = weighted_y.sum(1)  # (2816, 24)
     mae_loss = mean_absolute_error(inv_trans(test_y), inv_trans(weighted_y))
     mape_loss = mean_absolute_percentage_error(inv_trans(test_y), inv_trans(weighted_y))
     return mae_loss, mape_loss, act_sorted
+
